fb243f74675421f3b075ff8ee8d868bffe7c348a
Danil Vladimirov <d.vladimirov@limexltd.com>
2024-11-14 13:39:55 +0300

    feat/clear-core

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./core/redux/store";
-import App from "./app";
+import App from "./app/app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
